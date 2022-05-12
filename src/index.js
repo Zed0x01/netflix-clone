ba477dea8b6982665828769a19e0b9d0dbc99474
Zed0x01 <sherifashrafmedia@gmail.com>
2022-05-12 15:09:02 +0200

    Prettier

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom/client";
 import Netflix from "./Netflix";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <Netflix />
-);
+root.render(<Netflix />);
